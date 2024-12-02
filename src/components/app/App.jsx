72330389ea56f3159c40a2dd2668f9dc0415354b
Vladimir Khilko <vladimirhilko@gmail.com>
2024-12-02 21:33:13 +0300

    start marvel progect

---
+++ src/components/app/App.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/react-in-jsx-scope */
+import AppHeader from "../appHeader/AppHeader.jsx"
+import RandomChar from "../randomChar/RandomChar.jsx";
+import CharList from "../charList/CharList.jsx";
+import CharInfo from "../charInfo/CharInfo.jsx";
+
+import decoration from '../../resources/img/vision.png';
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppHeader/>
+            <main>
+                <RandomChar/>
+                <div className="char__content">
+                    <CharList/>
+                    <CharInfo/>
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision"/>
+            </main>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { createRoot } from "react-dom/client";
+import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
+import "./style/style.scss";
+
+const marvelService = new MarvelService();
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
+
+root.render(<App />);
