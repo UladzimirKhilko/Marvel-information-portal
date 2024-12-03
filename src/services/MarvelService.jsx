6ece5bca88b03ae222ab72a2146d146921d3093a
Vladimir Khilko <vladimirhilko@gmail.com>
2024-12-03 20:41:24 +0300

    Refactoring the RandomChar component

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -13,12 +13,29 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return  this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);       
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`); 
+        return res.data.results.map(this._transformCharacter);          
     }
 
-    getCharacter = (id) => {
-        return  this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);       
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);       
+    }
+
+    _transformCharacter = (char) => {
+        if (!char.description) {
+            char.description = 'There is no data available for this character.'
+        } else if (char.description.length > 200) {
+            char.description = char.description.substr(0, 197) + "...";
+        }
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path+'.'+char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
     }
 }
 
