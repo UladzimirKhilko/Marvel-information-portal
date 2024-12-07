7742beba78d3450df7b65a543898d8e434815db7
Vladimir Khilko <vladimirhilko@gmail.com>
2024-12-07 20:55:03 +0300

    added props-type

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
 import {Component} from 'react';
+
+import PropTypes from 'prop-types';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -114,6 +116,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
