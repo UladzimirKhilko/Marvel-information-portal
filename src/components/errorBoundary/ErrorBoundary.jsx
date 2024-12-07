eee33c688bf78b1e70acfadb74db576c8d6f84c9
Vladimir Khilko <vladimirhilko@gmail.com>
2024-12-07 17:54:08 +0300

    additional loading of characters

---
+++ src/components/errorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/require-render-return */
+import { Component } from "react";
+
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
+class ErrorBoundary extends Component {
+
+    state = {
+       error: false 
+    }
+    
+    componentDidCatch(error, errorInfo) {
+        console.log(error, errorInfo);
+        this.setState({
+            error: true
+        })
+    }
+
+    render() {
+        if(this.state.error) {
+            return <ErrorMessage/>
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
